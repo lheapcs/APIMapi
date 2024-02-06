f2f178e1808a57281d052ac6e468c3134c820884
Lewis <lheap_cs@protonmail.com>
2024-02-06 14:33:48 +0000

    Outlining logic with placeholders

--- APIMapi.py
+++ APIMapi.py
@@ -24,69 +24,71 @@ def start_main_parser():
     main_parser.add_argument("-w", "--wordlist", help="specify the location of a wordlist to fuzz with.")
     main_parser.add_argument("-o", "--output", help="output the fuzz result to the specified location.")
     main_parser.add_argument("-j", "--output_json", help="output in OpenAPI JSON format to the specified location.")
-    main_parser.add_argument("-ab", "--authentication_basic", help="authenticate API calls with provided basic details; supply just the username here.")
+    main_parser.add_argument("-ab", "--authentication_basic", help="authenticate API calls with provided basic details (supply just the username).")
     main_parser.add_argument("-ak", "--authentication_key", help="authenticate API calls with provided API key.")
 
     global user_args
     user_args = main_parser.parse_args()
 
-    # Below is an example to help with interacting with arguments
-    # Set Namespace object from argparse to a dictionary with 'vars(user_args)'
-    # If we're on the endpoint arg, skip it with 'continue'
-    # If the arg exists in the Namespace, print its value
-    #
-    #for arg in vars(user_args):
-    #   if arg == 'endpoint':
-    #        continue
-    #    if vars(user_args)[arg]:
-    #        print(vars(user_args)[arg])
-
-# Needs refining further
-# Function to log any error codes in the final result
-def log_error_response(status_code):
-    print(status_code)
-
-# Needs refining further
-def get_request(endpoint):
-    if endpoint == 'check':
-        current_endpoint = user_args.endpoint
-    else:
-        current_endpoint = endpoint
-    
+# Run a get request with the specified arguments to ensure the known endpoint is working.
+# This should only be called once due to user_pass.
+def check_request():   
     try:
         if user_args.authentication_basic:
             global user_pass 
-            user_pass == getpass() # Need a check somewhere to see if this already exists
-            response = requests.get(current_endpoint, auth=(user_args.authentication_basic, user_pass))
+            user_pass = getpass()
+            response = requests.get(user_args.endpoint, auth=(user_args.authentication_basic, user_pass))
         elif user_args.authentication_key:
-            response = requests.get(current_endpoint, headers={'X-APi-Key': user_args.authentication_key})
+            response = requests.get(user_args.endpoint, headers={'X-API-Key': user_args.authentication_key})
         else:
-            response = requests.get(current_endpoint)
+            response = requests.get(user_args.endpoint)
     except requests.exceptions.RequestException as err:
-        if endpoint == 'check':
-            print('Request to specified endpoint not successful; fuzz cannot begin. Request error:\n')
-            raise SystemExit(err)
-        else:
-            # Broken here, cannot access status code because this is an unsuccessful call
-            log_error_response(response.status_code)
-            return False
+        print('Request to specified endpoint not successful; fuzz cannot begin. Request error:\n')
+        raise SystemExit(err)
     return True
 
+# Placeholder
+def create_wordlist():
+    # If no argument, this equals a default wordlist specified here.
+    # If argument, find the wordlist in the folder specified.
+    return 'THE wordlist' 
+
+# Placeholder
+def get_fuzz():
+    print('GET fuzz. Got a wordlist: ' + wordlist)
+    if user_args.authentication_basic:
+        print('Can access user pass. Password: ' + user_pass)
+
+# Placeholder
+def post_fuzz():
+    print('POST fuzz. Got a wordlist: ' + wordlist)
+
+# Placeholder
+def options_fuzz():
+    print('OPTIONS fuzz. Got a wordlist: ' + wordlist)
+
+     
 # Placeholder    
-def begin_fuzz(request_test):
-    if request_test:
-        get_request('https://false') # Test to call without it being a check
-        print('we\'re starting then')    
+def fuzz_handler(check_request):
+    print('Starting fuzz')
+    global wordlist
+    wordlist = create_wordlist()
+    get_fuzz()
+    post_fuzz()
+    options_fuzz()
+    # Store the output if this argument is supplied. This will go to file at the end.
+    # Whether the above is true or not, output each fuzz attempt to the console.
 
 def main():
     start_main_parser()
 
     if not user_args.endpoint:
-        print(ascii + "\nusage: APIMapi [-h] [-w WORDLIST] [-o OUTPUT] [-j OUTPUT_JSON] [-ab AUTHENTICATION_BASIC] [-ak AUTHENTICATION_KEY] [endpoint]\n"
-            + "\nRun APIMapi with an endpoint to begin fuzzing or with the -h option to receive the full help menu.\n")
+        print(ascii + "\nRun APIMapi with an endpoint to begin fuzzing or with the -h option to receive the full help menu.\n"
+            + "\nusage: APIMapi [-h] [-w WORDLIST] [-o OUTPUT] [-j OUTPUT_JSON] [-ab AUTHENTICATION_BASIC] [-ak AUTHENTICATION_KEY] [endpoint]\n"
+            )
     else:
         # Start a fuzz but only if the endpoint specified is not erroring
-        begin_fuzz(get_request('check')) # Specify check so we know it's not a get call for the fuzz
+        fuzz_handler(check_request())
     
 
 if __name__ == "__main__":
