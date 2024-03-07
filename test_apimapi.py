f1298550299e136573f1842d409f344f911874d4
lheap_cs <lheap_cs@protonmail.com>
2024-03-07 16:59:31 +0000

    Start of looking into tests

---
+++ test_apimapi.py
@@ -0,0 +1,9 @@
+import APIMapi
+import unittest
+
+class TestApiMapi(unittest.TestCase):
+    
+    def test_vfuzz(self):
+        # Need to call vfuzz here with an endpoint.
+        # First of all, code needs a refactor to remove the global user args.
+        pass
